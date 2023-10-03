b868fbd07467412473308837864837fe8f0a5be8
zo0r <z@zo0r.me>
2023-10-03 19:57:27 -0400

    Update templates

--- templates/react/src/server/script.js
+++ templates/react/src/server/script.js
@@ -1,10 +1,8 @@
 import { Command, initEngine } from "@verza/sdk";
 
 export default async function script() {
-  const VERZA_TOKEN = process.env.VERZA_TOKEN;
-
   const engine = await initEngine({
-    accessToken: VERZA_TOKEN,
+    accessToken: "ACCESS_TOKEN",
   });
 
   engine.commands.register(
